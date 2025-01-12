7ce183a51c79984cb028e365a290860423c3edab
Tomas53 <48329351+Tomas53@users.noreply.github.com>
2025-01-12 13:52:01 +0200

    tomas-06 added user repo (#9)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,21 @@
 import repositories.DatabaseRepo;
+import repositories.UserRepo;
 import utils.JsonFileConverter;
 
 public class Main {
     public static void main(String[] args) {
         JsonFileConverter jsonFileConverter = new JsonFileConverter();
-        jsonFileConverter.getAllUsersFromJson().forEach(System.out::println);
+        //jsonFileConverter.getAllUsersFromJson().forEach(System.out::println);
         jsonFileConverter.getAllMoviesFromJson();
         jsonFileConverter.getAllPlatformsFromJson();
         DatabaseRepo databaseRepo=new DatabaseRepo();
-        databaseRepo.deleteTables();
+//        databaseRepo.deleteTables();
+        UserRepo userRepo=new UserRepo();
         databaseRepo.createTables();
+//        for (int i = 0; i <jsonFileConverter.getAllUsersFromJson().size() ; i++) {
+//            userRepo.insertUser(jsonFileConverter.getAllUsersFromJson().get(i));
+//            System.out.println(jsonFileConverter.getAllUsersFromJson().get(i));
+//        }
+
     }
 }
--- src/main/java/repositories/UserRepo.java
+++ src/main/java/repositories/UserRepo.java
@@ -1,4 +1,28 @@
 package repositories;
 
+import dto.UserDto;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class UserRepo {
+    public void insertUser(UserDto userDto){
+        String SQL= """
+                insert into Users (name, date_of_birth, email, platformid values (?, ?, ?, ?)
+                """;
+
+        try(Connection connection=DatabaseRepo.getConnection()){
+            PreparedStatement preparedStatement=connection.prepareStatement(SQL);
+            preparedStatement.setString(1, userDto.getName());
+            preparedStatement.setDate(2, java.sql.Date.valueOf(userDto.getDateOfBirth()));
+            preparedStatement.setString(3, userDto.getEmail());
+            preparedStatement.setInt(4, userDto.getPlatformId());
+
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
