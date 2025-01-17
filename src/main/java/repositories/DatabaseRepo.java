207d3a10a3f5b4cd59d5230e13c86ee65db946cc
12881288 <gustaitis.jon@gmail.com>
2025-01-17 20:23:48 +0200

    bonus-cli refactored code for better readability and used AI to refactor it further

--- src/main/java/repositories/DatabaseRepo.java
+++ src/main/java/repositories/DatabaseRepo.java
@@ -13,8 +13,8 @@ import java.util.Map;
 
 public class DatabaseRepo {
     private static final String URL = "jdbc:postgresql://localhost:5432/moviesApp";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "postgres";
+    private static final String USER = "*";
+    private static final String PASSWORD = "*";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
